e67236b326d1a9b9241711aa07dc422c884bd33c
Amr Mostafa Elshehaly <amshehaly@gmail.com>
2022-04-07 17:07:05 +0200

    added our sign in component

--- myshop/src/utils/firebase/firebase.utils.js
+++ myshop/src/utils/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import {initializeApp} from 'firebase/app';
 // import { getAnalytics } from "firebase/analytics";
 import {getAuth,signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
 
 
 const firebaseConfig = {
@@ -25,3 +26,33 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth)=> {
+
+    const userDocRef = doc(db, 'users', userAuth.uid)
+
+    console.log(userDocRef)
+
+    const userSnapshot = await getDoc(userDocRef)
+
+    // console.log(userSnapshot.exists())
+
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth
+        const createdAt = new Date()
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log("Error creating a user",error)
+        }
+    }
+
+    return userDocRef
+}       
